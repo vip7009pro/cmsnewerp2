6f7d95e674e192b8d5500a3d35fd8596f53be515
vip7009pro <hung1893@gmail.com>
2022-07-28 18:28:02 +0700

    Sửa chút hàm global

--- src/api/GlobalFunction.tsx
+++ src/api/GlobalFunction.tsx
@@ -1,6 +1,6 @@
 import * as XLSX from 'xlsx';
 
-const SaveExcel = (data: any, title: string) => {
+export const SaveExcel = (data: any, title: string) => {
     const worksheet = XLSX.utils.json_to_sheet(data);
     const workbook = XLSX.utils.book_new();
     XLSX.utils.book_append_sheet(workbook, worksheet, "Sheet1");
@@ -9,4 +9,18 @@ const SaveExcel = (data: any, title: string) => {
     XLSX.writeFile(workbook, `${title}.xlsx`);
 }
 
-export default SaveExcel;
+export const readUploadFile = (e:any) => {
+    e.preventDefault();
+    if (e.target.files) {
+        const reader = new FileReader();
+        reader.onload = (e:any) => {
+            const data = e.target.result;
+            const workbook = XLSX.read(data, { type: "array" });
+            const sheetName = workbook.SheetNames[0];
+            const worksheet = workbook.Sheets[sheetName];
+            const json = XLSX.utils.sheet_to_json(worksheet);           
+            return json;
+        };
+        reader.readAsArrayBuffer(e.target.files[0]);
+    }
+}
