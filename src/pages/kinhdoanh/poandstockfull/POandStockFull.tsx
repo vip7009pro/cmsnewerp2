03e3dec83d43f87566e8256ac5a639e479e2846e
vip7009pro <hung1893@gmail.com>
2022-08-13 08:16:55 +0700

    Thêm ycsx component

--- src/pages/kinhdoanh/poandstockfull/POandStockFull.tsx
+++ src/pages/kinhdoanh/poandstockfull/POandStockFull.tsx
@@ -1,19 +1,28 @@
 import YCSXComponent from '../ycsxmanager/YCSXComponent/YCSXComponent'
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import { useReactToPrint } from 'react-to-print';
 
 const POandStockFull = () => {  
+  const [ycsxno, setYCSXNO] = useState<string>('');
+
+
   const componentRef = useRef(null);
   const handlePrint = useReactToPrint({
     content :  () => componentRef.current,
   });
-
-  const ycsxlist:Array<number> = [1,2,3,4,5,6,7,8]
-  const renderYCSX = () => {
-    return ycsxlist.map((element,index)=> <YCSXComponent/>)
+  interface YCSXLIST {
+    PROD_REQUEST_NO: string,
+    G_CODE: string
   }
+  const ycsxlist:Array<YCSXLIST> = [{PROD_REQUEST_NO: '2HC0020',G_CODE:'7C05142A'},{PROD_REQUEST_NO: '2HC0019',G_CODE:'7A07674A'},{PROD_REQUEST_NO: '2HA0051',G_CODE:'7A05268C'},{PROD_REQUEST_NO: '2HC0019',G_CODE:'7A07674A'},{PROD_REQUEST_NO: '2HC0019',G_CODE:'7A07674A'}]
+  const renderYCSX = () => {
+    return ycsxlist.map((element,index)=> <YCSXComponent key={index} PROD_REQUEST_NO={element.PROD_REQUEST_NO} G_CODE={element.G_CODE}/>)
+  }
+
+
   return (
     <div>POandStockFull    
+      <input type="text" value ={ycsxno} onChange={(e)=> {setYCSXNO(e.target.value);}}></input>
        <button onClick={handlePrint}>Print YCSX</button>  
        <div ref={componentRef} >
           {renderYCSX()}
