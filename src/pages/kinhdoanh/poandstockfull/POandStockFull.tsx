e44e91a1bd45a6cea930ea171009c4496fe60bbc
vip7009pro <hung1893@gmail.com>
2022-08-15 07:55:54 +0700

    reset PO and Stock full component

--- src/pages/kinhdoanh/poandstockfull/POandStockFull.tsx
+++ src/pages/kinhdoanh/poandstockfull/POandStockFull.tsx
@@ -1,54 +1,41 @@
-import YCSXComponent from '../ycsxmanager/YCSXComponent/YCSXComponent'
-import  { ReactElement, useRef, useState } from 'react';
-import { useReactToPrint } from 'react-to-print';
+import { useState } from 'react';
+import Swal from 'sweetalert2';
+import { generalQuery } from '../../../api/Api';
 import './POandStockFull.scss'
-import DrawComponent from '../ycsxmanager/DrawComponent/DrawComponent';
+const axios = require('axios').default;
 
 const POandStockFull = () => {  
-  const [ycsxno, setYCSXNO] = useState<string>('');
-  const [ycsxlistrender, setYCSXListRender] = useState<Array<ReactElement>>();
   
+  const [file, setFile] = useState<any>();
+  const [fileName, setFileName] = useState("");
 
-  const componentRef = useRef(null);
-  const handlePrint = useReactToPrint({
-    content :  () => componentRef.current,
-  });
-  interface YCSXLIST {
-    PROD_REQUEST_NO: string,
-    G_CODE: string
-  }
-  const ycsxlist:Array<YCSXLIST> = [{PROD_REQUEST_NO: '2HC0020',G_CODE:'7C05142A'},{PROD_REQUEST_NO: '2HC0019',G_CODE:'7A07674A'},{PROD_REQUEST_NO: '2HA0051',G_CODE:'7A05268C'}]
+  const saveFile = (e: any) => {
+    setFile(e.target.files[0]);    
+    setFileName(e.target.files[0].name);
+    console.log(e.target.files[0]);
+  };
 
-  const renderYCSX = () => {
-    return  ycsxlist.map((element,index)=><DrawComponent key={index}/> )
-  }
+  const uploadFile = async (e:any) => {
+    const formData = new FormData();
+    formData.append("file", file);
+    formData.append("fileName", fileName);
+    try {
+      const res = await axios.post(
+        "http://14.160.33.94:3007/api",
+        formData
+      );
+      console.log(res);
+    } catch (ex) {
+      console.log(ex);
+    }
+  };
 
   return (
     <div>
-      POandStockFull
-      <input
-        type='text'
-        value={ycsxno}
-        onChange={(e) => {
-          setYCSXNO(e.target.value);
-        }}
-      ></input>
-      <button onClick={handlePrint}>Print YCSX</button>
-      <button
-        onClick={() => {
-          setYCSXListRender(renderYCSX());
-        }}
-      >
-        Render YCSX
-      </button>
-      <div ref={componentRef}>
-        <div>
-          {
-            renderYCSX()
-          }
-        </div>
-      </div>
-      <div></div>
+      <div className="App">
+          <input type="file" onChange={saveFile} />
+          <button onClick={uploadFile}>Upload</button>
+      </div>     
     </div>
   );
 }
