0ee30dda24bf49326b2a94b1b2d4b77e94427fd8
vip7009pro <hung1893@gmail.com>
2024-02-05 11:22:23 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -592,7 +592,7 @@ function App() {
                               width: "100%",
                               height: "100%",
                               borderRadius: 8,
-                              ...springs,
+                              /* ...springs, */
                             }}
                           >
                             <Home />
