88bc2c7244f635f94df1a5962479f739e3d402b4
vip7009pro <hung1893@gmail.com>
2025-02-03 16:50:40 +0700

    add post manager funtions

--- src/api/GlobalFunction.tsx
+++ src/api/GlobalFunction.tsx
@@ -36,6 +36,7 @@ import {
   MAT_DOC_DATA,
   POBALANCETDYCSX,
   PONOLIST,
+  POST_DATA,
   POTableData,
   PRICEWITHMOQ,
   PROD_OVER_DATA,
@@ -7255,3 +7256,61 @@ export const f_getProductionPlanLeadTimeCapaData = async (PLAN_DATE: string) => 
       });
       return kq;
   }
+export const  f_fetchPostList = async () => {
+  let kq: POST_DATA[] = [];
+    try {
+      let res = await generalQuery('loadPost', {});
+      //console.log(res);
+      if (res.data.tk_status !== 'NG') {
+        //console.log(res.data.data);
+        let loaded_data: POST_DATA[] = res.data.data.map((ele: POST_DATA, index: number) => {
+          return {
+            ...ele,
+            INS_DATE: moment.utc(ele.INS_DATE).format('YYYY-MM-DD HH:mm:ss'),
+            UPD_DATE: moment.utc(ele.UPD_DATE).format('YYYY-MM-DD HH:mm:ss'),
+            id: index
+          }
+        })
+        kq = loaded_data;       
+      } else {
+        console.log('fetch error');
+      }
+    } catch (error) {
+      console.log(error);
+    }
+    return kq;
+  };
+
+export const f_updatePostData = async (DATA: POST_DATA) => {
+  //console.log(DATA)
+  let kq: string = '';
+  await generalQuery("updatePost", DATA)
+    .then((response) => {
+      if (response.data.tk_status !== "NG") {
+        
+      }
+      else {
+        kq = response.data.message;
+      }
+    })
+    .catch((error) => {
+  
+})
+}
+
+export const f_deletePostData = async (DATA: POST_DATA) => {
+  //console.log(DATA)
+  let kq: string = '';
+  await generalQuery("deletePost", DATA)
+    .then((response) => {
+      if (response.data.tk_status !== "NG") {
+        
+      }
+      else {
+        kq = response.data.message;
+      }
+    })
+    .catch((error) => {
+  
+})
+}
