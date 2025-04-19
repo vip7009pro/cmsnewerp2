844f7a23b501d2cfe67b609bc4d64f20e13b02b4
vip7009pro <hung1893@gmail.com>
2025-04-19 15:53:34 +0700

    sua hien thi cho worker

--- src/pages/qc/pqc/PQC.tsx
+++ src/pages/qc/pqc/PQC.tsx
@@ -8,45 +8,46 @@ import PATROL from "../../sx/PATROL/PATROL";
 import PQC_REPORT from "./PQC_REPORT";
 import QLGN from "../../rnd/quanlygiaonhandaofilm/QLGN";
 import MyTabs from "../../../components/MyTab/MyTab";
+import { getUserData } from "../../../api/Api";
 
 const PQC = () => {
   useEffect(() => {}, []);
   return (
     <div className="pqc">
       <MyTabs  defaultActiveTab={0}>
-        <MyTabs.Tab title={"LINEQC"}>
+        {getUserData()?.SUBDEPTNAME?.includes("PQC")  &&<MyTabs.Tab title={"LINEQC"}>
           <LINEQC />
-        </MyTabs.Tab>
-        <MyTabs.Tab title={"DATA PQC"}>
+        </MyTabs.Tab>}
+        {getUserData()?.JOB_NAME !=='Worker' && <MyTabs.Tab title={"DATA PQC"}>
           <div className="trapqc">
             <TRAPQC />
           </div>
-        </MyTabs.Tab>
-        <MyTabs.Tab title={"PQC1-SETTING"}>
+        </MyTabs.Tab>}
+         {getUserData()?.JOB_NAME !=='Worker' &&  <MyTabs.Tab title={"PQC1-SETTING"}>
           <div className="trapqc">
             <PQC1 />
           </div>
-        </MyTabs.Tab>
-        <MyTabs.Tab title={"PQC3-DEFECT"}>
+        </MyTabs.Tab>}
+         {getUserData()?.JOB_NAME !=='Worker' &&  <MyTabs.Tab title={"PQC3-DEFECT"}>
           <div className="trapqc">
             <PQC3 />
           </div>
-        </MyTabs.Tab>
-        <MyTabs.Tab title={"PATROL"}>
+        </MyTabs.Tab>}
+         {getUserData()?.JOB_NAME !=='Worker' &&  <MyTabs.Tab title={"PATROL"}>
           <div className="trapqc">
             <PATROL />
           </div>
-        </MyTabs.Tab>
-        <MyTabs.Tab title={"Giao Nhận Dao Film"}>
+        </MyTabs.Tab>}
+         {getUserData()?.JOB_NAME !=='Worker' &&  <MyTabs.Tab title={"Giao Nhận Dao Film"}>
           <div className="trapqc">
             <QLGN />
           </div>
-        </MyTabs.Tab>
-        <MyTabs.Tab title={"Báo Cáo"}>
+        </MyTabs.Tab>}
+         {getUserData()?.JOB_NAME !=='Worker' &&  <MyTabs.Tab title={"Báo Cáo"}>
           <div className="trapqc">
             <PQC_REPORT />
           </div>
-        </MyTabs.Tab>
+        </MyTabs.Tab>}
       </MyTabs>
     </div>
   );
