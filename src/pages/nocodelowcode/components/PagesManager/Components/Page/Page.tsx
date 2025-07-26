e6ef869e233c22e4affb6c3319e50debf92b01fc
vip7009pro <hung1893@gmail.com>
2025-07-26 16:15:58 +0700

    update page manager

--- src/pages/nocodelowcode/components/PagesManager/Components/Page/Page.tsx
+++ src/pages/nocodelowcode/components/PagesManager/Components/Page/Page.tsx
@@ -1,36 +1,26 @@
-import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom';
+import { useEffect } from 'react';
 import { use_f_loadComponents } from '../../../../utils/nocodelowcodeHooks';
 import FormComponent from '../Form/FormComponent';
 import TableComponent from '../Table/TableComponent';
-
 export default function PageShow({ pageId }: { pageId: string | number }) {
-      const {
-        data: components,
-        loading: loadingComponents,
-        error: errorComponents,
-        triggerFetch: fetchComponents,
-        triggerFetchWithParams: triggerFetchComponentsWithParams
-      } = use_f_loadComponents({ PageID: pageId });
-   
-    useEffect(() => {
-      fetchComponents();
-    }, [pageId]);
-
-    return (
-      <div
-        style={{
-          display: 'grid',
-          gridTemplateColumns: '1fr 1fr',
-          gap: '20px',
-          padding: '20px'
-        }}
-      >
-        {components.map(component => { 
-            return (
+  const { data: components, loading: loadingComponents, error: errorComponents, triggerFetch: fetchComponents, triggerFetchWithParams: triggerFetchComponentsWithParams } = use_f_loadComponents({ PageID: pageId });
+  useEffect(() => {
+    fetchComponents();
+  }, [pageId]);
+  return (
+    <div
+      style={{
+        display: 'grid',
+        gridTemplateColumns: '1fr 1fr',
+        gap: '20px',
+        padding: '20px',
+      }}
+    >
+      {components.map((component) => {
+        return (
           <div
             key={component.ComponentID}
-            style={{                
+            style={{
               gridColumn: component.GridWidth !== 'full' ? 'span 2' : 'span 1',
               gridRow: `span 1`,
               minHeight: component.Height || 200,
@@ -41,20 +31,18 @@ export default function PageShow({ pageId }: { pageId: string | number }) {
             {component.ComponentType === 'Form' && (
               <div style={{ display: 'flex', flexDirection: 'column', gap: '5px', width: '100%', height: '100%' }}>
                 <h3>{component.ComponentName}</h3>
-                <FormComponent formId={component.ReferenceID ?? 0}/>
+                <FormComponent formId={component.ReferenceID ?? 0} />
               </div>
             )}
             {component.ComponentType === 'DataTable' && (
               <div style={{ display: 'flex', flexDirection: 'column', gap: '5px', width: '100%', height: '100%' }}>
                 <h3>{component.ComponentName}</h3>
-                <TableComponent formID={component.ReferenceID ?? 0}/>
+                <TableComponent formID={component.ReferenceID ?? 0} />
               </div>
             )}
           </div>
-        )})}
-      </div>
-    );
-  };
-  
-
-  
+        );
+      })}
+    </div>
+  );
+}
